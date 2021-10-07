418925633bef139b56afcc21c880c519af9da440
Antoine Fabri <antoine.fabri@gmail.com>
2021-10-07 10:24:36 +0200

    Use better variable names

--- R/refactor_impl.r
+++ R/refactor_impl.r
@@ -1,46 +1,46 @@
-refactor_impl <- function(original, refactored, refactor.value, refactor.time, refactor.env, refactor.waldo, pf) {
+refactor_impl <- function(original, refactored, refactor.value, refactor.time, refactor.env, refactor.waldo, src_env) {
 
   ## record env before modifications
-  original_env_as_list  <- clone_env(pf)
-  original_var_nms  <- names(original_env_as_list)
+  original_env  <- clone_env(src_env)
+  original_var_nms  <- names(original_env)
 
 
   if(refactor.time) {
-    original_time     <- system.time(original_value   <- eval(original, pf))[["elapsed"]]
+    original_time     <- system.time(original_value   <- eval(original, src_env))[["elapsed"]]
   } else {
-    original_value   <- eval(original, pf)
+    original_value   <- eval(original, src_env)
   }
 
   ## record env after modifications made by original code
-  new_env1_as_list   <- clone_env(pf)
-  new_var_nms       <- names(new_env1_as_list)
+  new_env1    <- clone_env(src_env)
+  new_var_nms <- names(new_env1)
 
   ## reinitiate env
-  rm(list=setdiff(new_var_nms, original_var_nms), envir = pf)
+  rm(list=setdiff(new_var_nms, original_var_nms), envir = src_env)
   for (var_nm in intersect(new_var_nms, original_var_nms)) {
-    promise_lgl <- pryr:::is_promise2(as.symbol(var_nm), env = pf)
+    promise_lgl <- is_unevaled_promise(var_nm, src_env)
     if(promise_lgl) {
-      promise_expr <- pryr:::promise_code(var_nm, pf)
-      promise_env <- pryr:::promise_env(var_nm, pf)
+      promise_expr <- pryr:::promise_code(var_nm, src_env)
+      promise_env  <- pryr:::promise_env(var_nm, src_env)
       # Assign this expression as a promise (delayed assignment) in our
       # cloned environment
       eval(bquote(
-        delayedAssign(var_nm, .(promise_expr), eval.env = promise_env, assign.env = pf)))
+        delayedAssign(var_nm, .(promise_expr), eval.env = promise_env, assign.env = src_env)))
     } else {
-      pf[[var_nm]] <- original_env_as_list[[var_nm]]
+      src_env[[var_nm]] <- original_env[[var_nm]]
     }
   }
 
   if(refactor.time) {
-    refactored_time   <- system.time(refactored_value <- eval(refactored, pf))[["elapsed"]]
+    refactored_time   <- system.time(refactored_value <- eval(refactored, src_env))[["elapsed"]]
   } else {
-    refactored_value <- eval(refactored, pf)
+    refactored_value <- eval(refactored, src_env)
   }
 
   ## record env after modifications made by refactored code
-  new_env2_as_list   <- clone_env(pf)
+  new_env2   <- clone_env(src_env)
 
-  if(refactor.value && !identical(original_value, refactored_value)) {
+  if(refactor.value && !identical2(original_value, refactored_value)) {
     stop("The refactored expression returns a different value than the original one.\n\n",
          if(refactor.waldo)
            paste(waldo::compare(
@@ -52,9 +52,9 @@ refactor_impl <- function(original, refactored, refactor.value, refactor.time, r
   }
 
   if(refactor.env) {
-    vars1 <- ls(envir = new_env1_as_list, all.names = TRUE)
-    vars2 <- ls(envir = new_env2_as_list, all.names = TRUE)
-    if(!identical(vars1, vars2)) {
+    vars1 <- ls(envir = new_env1, all.names = TRUE)
+    vars2 <- ls(envir = new_env2, all.names = TRUE)
+    if(!identical2(vars1, vars2)) {
       setdiff1 <- setdiff(vars1, vars2)
       msg1 <- paste0(
         "Some variables defined in the original code, were not found in the ",
@@ -68,19 +68,19 @@ refactor_impl <- function(original, refactored, refactor.value, refactor.time, r
       stop(paste(c(msg1, msg2), collapse = "\n"), call. = FALSE)
     }
     for(var_nm in vars1) {
-      promise_lgl <- pryr:::is_promise2(as.symbol(var_nm), env = new_env1_as_list)
+      promise_lgl <- is_unevaled_promise(var_nm, env = new_env1)
       if(promise_lgl) {
-        if(! pryr:::is_promise2(var_nm, new_env2_as_list))
-          stop("`", var_nm, "` is a promise in the original code, but is evaluated",
+        if(! is_unevaled_promise(var_nm, new_env2))
+          stop("`", var_nm, "` is an unevaled promise in the original code, but is evaluated",
                "in the refactored code")
 
         promise_info1 <- list(
-          code = pryr:::promise_code(var_nm, new_env1_as_list),
-          env = pryr:::promise_env(var_nm, new_env1_as_list))
+          code = pryr:::promise_code(var_nm, new_env1),
+          env = pryr:::promise_env(var_nm, new_env1))
 
         promise_info2 <- list(
-          code = pryr:::promise_code(var_nm, new_env2_as_list),
-          env = pryr:::promise_env(var_nm, new_env2_as_list))
+          code = pryr:::promise_code(var_nm, new_env2),
+          env = pryr:::promise_env(var_nm, new_env2))
 
         if(!identical(promise_info1, promise_info2)) {
           stop("The promise `var_nm` is different in original and refactored code \n",
@@ -90,12 +90,12 @@ refactor_impl <- function(original, refactored, refactor.value, refactor.time, r
                call. = FALSE)
         }
       } else {
-        if(pryr:::is_promise2(var_nm, new_env2_as_list))
-          stop("`", var_nm, "` is a promise in the refactored code, but is evaluated",
+        if(is_unevaled_promise(var_nm, new_env2))
+          stop("`", var_nm, "` is an unevaled promise in the refactored code, but is evaluated",
                "in the original code")
-        val1 <- new_env1_as_list[[var_nm]]
-        val2 <- new_env2_as_list[[var_nm]]
-        if(!identical(val1, val2)) {
+        val1 <- new_env1[[var_nm]]
+        val2 <- new_env2[[var_nm]]
+        if(!identical2(val1, val2)) {
           stop("The variable `", var_nm, "` is bound to a  different value ",
                "after the original and refactored code\n",
                if(refactor.waldo)

0cfc0720eaeedadceeae0ab2611fb22b09dd9cba
Antoine Fabri <antoine.fabri@gmail.com>
2022-03-22 09:21:21 +0100

    Improve clone_env

--- R/utils.R
+++ R/utils.R
@@ -1,16 +1,23 @@
+gfn <- getFromNamespace
+promise_code <- gfn("promise_code", "pryr")
+promise_env <- gfn("promise_env", "pryr")
+promise_evaled <- gfn("promise_evaled", "pryr")
+is_promise2 <- gfn("is_promise2", "pryr")
+
 clone_env <- function(env, deep = FALSE) {
   # create new environment with same parent
   clone <- new.env(parent = parent.env(env))
   for(obj in ls(env, all.names = TRUE)) {
     promise_lgl <- is_unevaled_promise(as.symbol(obj), env = env)
     if(promise_lgl) {
-      # fetch promise expression, we use bquote to feed the right unquoted
-      # value to substitute
-      promise_expr <- eval(bquote(substitute(.(as.symbol(obj)), env = env)))
+      # fetch promise expression and env
+      promise_expr <- promise_code(obj, env)
+      promise_env  <- promise_env(obj, env)
+
       # Assign this expression as a promise (delayed assignment) in our
       # cloned environment
       eval(bquote(
-        delayedAssign(obj, .(promise_expr), eval.env = env, assign.env = clone)))
+        delayedAssign(obj, .(promise_expr), eval.env = promise_env, assign.env = clone)))
     } else {
       obj_val <- get(obj, envir = env)
       if(is.environment(obj_val) && deep) {
@@ -25,7 +32,7 @@ clone_env <- function(env, deep = FALSE) {
 }
 
 is_unevaled_promise <- function(name, env) {
-  pryr:::is_promise2(name, env) && !pryr:::promise_evaled(name, env)
+  is_promise2(name, env) && !promise_evaled(name, env)
 }
 
 identical2 <- function(target, current, ...) {
